e3fb1198d760c2499b1bccc9ed98c08e8957d840
Adarsh Jha <jhaadarsh195@gmail.com>
2025-04-03 17:04:18 +0530

    Added TypeScript Loading Animation Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./components/Homepage/Homepage";
 import BackToTop from "./components/BackToTop/BackToTop";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingAnimation from "./components/LoadingReBuyIt/animation";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); 
+  }, []);
+
+  if (isLoading) return <LoadingAnimation />;
+
   return (
     <Router>
       <Navbar />
