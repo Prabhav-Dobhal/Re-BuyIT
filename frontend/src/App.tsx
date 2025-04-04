3a01e6f49ec9f1b5a59b7f388ad4325b0f3d86bc
hogalalagammu <ggxgaurav2020@gmail.com>
2025-04-04 14:48:21 +0530

    Product detail from my side is done and this is for amman to clone

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer/Footer";
 import HomePage from "./components/Homepage/Homepage";
 import BackToTop from "./components/BackToTop/BackToTop";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductPage from "./components/ProductDetails/ProductPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/productDetails" element={<ProductPage />} />
       </Routes>
       <BackToTop />
       <Footer />
