e1608f3096178d816513a9c50603af8494e47a18
Piyush Kathait <kathaitpiyush1@gmail.com>
2025-04-05 20:29:00 +0530

    Feat: added backToTop component

--- frontend/src/components/BackToTop/BackToTop.tsx
+++ frontend/src/components/BackToTop/BackToTop.tsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useTheme } from '../../hooks/useTheme';
+import './BackToTop.css';
 const BackToTop = () => {
+  const [visible, setVisible] = useState(false);
+  const { theme } = useTheme();
+  useEffect(() => {
+    const toggleVisibility = () => {
+      setVisible(window.scrollY > window.innerHeight / 2);
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  if (!visible) return null;
+
   return (
-    <>
-      <h1>BackToTop</h1>
-    </>
+    <button onClick={scrollToTop} className={`back-to-top-button ${theme  === 'dark' ? 'dark' : ''}`}>
+      <img src="../../src/assets/chevron-up.svg" alt="Chevron Up" className="back-to-top-icon" />
+      Back to Top
+    </button>
+
   );
 };
+
 export default BackToTop;
+// 
